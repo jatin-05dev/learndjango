aa6e35abd095cf1ad2a1e11e466f1926b530d64c
jatin-05dev <jatindev214@gmail.com>
2026-02-19 21:11:12 +0530

    changes'

--- newproject/myproject/myapp/forms.py
+++ newproject/myproject/myapp/forms.py
@@ -7,11 +7,4 @@ class EmpForm(forms.ModelForm):
         model = emp
         fields = "__all__"
 
-        widgets = {
-            'fname': forms.TextInput(attrs={'class': 'input'}),
-            'lname': forms.TextInput(attrs={'class': 'input'}),
-            'email': forms.EmailInput(attrs={'class': 'input'}),
-            'mobile': forms.TextInput(attrs={'class': 'input'}),
-            'DOB': forms.DateInput(attrs={'type': 'date', 'class': 'input'}),
-            'gender': forms.RadioSelect(),
-        }
+        
