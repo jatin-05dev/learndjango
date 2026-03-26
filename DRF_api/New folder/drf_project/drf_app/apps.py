b32bf98348a7bb1f4cb6f0c5f4ed8c9d7b146b6b
jatin-05dev <jatindev214@gmail.com>
2026-03-26 16:55:21 +0530

    api

--- DRF_api/New folder/drf_project/drf_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrfAppConfig(AppConfig):
-    name = 'drf_app'
--- DRF_api/pr/mypr/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from myapp import views
-
-urlpatterns = [
-    path('stu_list/',views.stu_list),
-    path('stu_details/<int:pk>/',views.stu_details),
-]
